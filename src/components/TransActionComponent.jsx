a72c466c0dd77377eb0ed1f103a6598a83b4eeb5
AmirAlipoor-ir <89448149+AmirAlipoor-ir@users.noreply.github.com>
2023-11-12 13:43:41 +0330

    add List of expense & income

--- src/components/TransActionComponent.jsx
+++ src/components/TransActionComponent.jsx
@@ -2,11 +2,11 @@ import React from "react";
 
 function TransActionComponent({ note }) {
   return (
-    <div>
+    <div className="">
       {note.map((i,index) => (
-        <section key={index}>
+        <section key={index} className="mb-2">
           <div
-            className={`flex justify-between bg-gray-100 py-1 px-1 rounded-md border-r-4 ${
+            className={`flex justify-between bg-gray-100 py-2 px-1 rounded-md border-r-4 ${
               i.type === "income" ? " border-green-600" : "border-red-600"
             }`}
           >

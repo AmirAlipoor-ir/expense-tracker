1759b5f9c4b888afba0b88cbb5d3048b1a6426cf
AmirAlipoor-ir <89448149+AmirAlipoor-ir@users.noreply.github.com>
2023-11-20 00:20:40 +0330

    add style

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import ExpenseApp from "./components/ExpenseApp";
 function App() {
   return (
     <div className="bg-gray-100 h-screen px-auto pt-3 font-sans font-normal text-lg">
-      <h1 className="mx-auto w-fit font-semibold pb-1 text-2xl ">Expense tracker</h1>
+      <h1 className="mx-auto w-fit font-semibold pb-1 text-2xl sm:text-4xl sm:pb-3">Expense tracker</h1>
       <ExpenseApp/>
     </div>
   );
